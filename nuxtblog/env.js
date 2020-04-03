84f2737d65e478046ba0765fef3abfe2cb7aa9a1
yqxshiki <yiqixing@yeah.net>
2020-04-03 13:25:47 +0800

    nuxtjs

--- nuxtblog/env.js
+++ nuxtblog/env.js
@@ -2,7 +2,7 @@
 module.exports = {
     dev: {
         MODE: 'development',
-        ENV_API: "http://49.232.96.54:9999/web/api"
+        ENV_API: "//49.232.96.54:9999/web/api"
     },
     pro: {
         MODE: 'production',
